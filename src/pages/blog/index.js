da32e2342ab0507eb7e2382bdcee43e10a1c4bb3
Kyle Shook <kshook@mural.co>
2021-01-29 10:58:23 -0600

    Forgot to add image...

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/muralusers.jpg')`,
+            backgroundImage: `url('/img/MuralTeam.png')`,
           }}
         >
           <h1
