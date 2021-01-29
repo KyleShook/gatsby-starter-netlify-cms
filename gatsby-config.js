9a0463d9613df35873def0621b69e79bf754f430
Kyle Shook <kshook@mural.co>
2021-01-29 15:52:49 -0600

    config filepath fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
-        ignore: 'prismjs/',
+        ignore: ['prismjs/'],
         develop: true, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
