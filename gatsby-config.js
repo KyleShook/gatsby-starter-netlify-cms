dfe7c1a82f447b96555ab5e0b067519b77fa0d54
Kyle Shook <kshook@mural.co>
2021-01-29 13:51:30 -0600

    another prism update

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,18 +37,8 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (e.g. <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (e.g. for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
-            
-            },
+            resolve: 'gatsby-remark-prismjs',
+            options: {},
           },
           {
             resolve: 'gatsby-remark-relative-images',
